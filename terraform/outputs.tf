467d39114c41e22494fa65a4ed0cf1fa2ea726f4
gopi <148228946+gopi8324@users.noreply.github.com>
2024-07-03 15:55:52 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,3 @@
 output "web-ip" {
-  value = aws_instance.web.public_ip
-}
-
-output "web-url" {
-  value = "http://${aws_instance.web.public_ip}/preschool"
-
-}
-
-output "web-url-other" {
-  value = format("http://%s/preschool", aws_instance.web.public_ip)
+  value = aws_instance.slim.public_ip
 }
